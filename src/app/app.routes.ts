d679f5c87e032cf6d06af5dc94528d423edaa318
Ratovonirina <eric.ratovonirina@gmail.com>
2024-02-27 23:51:30 +0300

    Init Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AssignmentsComponent } from './assignments/assignments.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo:'home', pathMatch: 'full'},
+    { path: 'home', component: AssignmentsComponent},
+    { path: 'add', component: AssignmentsComponent}
+];
